2c1a47a5ef92edcc6718ab8b6ecc4da9f4d1d980
Ashwin Pc <Ashwin Pc@DESKTOP-NRN0EQ3>
2025-11-08 12:15:49 +0530

    Dependency Injection

---
+++ DependencyInjection/IGreetingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    internal interface IGreetingService
+    {
+        void Greet(string name);
+    }
+}
--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+//mem : MEDA->MEDI->M.E.DI->Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+//create bag or container for collection
+
+//services.AddTransient<IGreetingService, GreetingService>();//per injection
+//services.AddScoped<IGreetingService, GreetingService>();//per request
+services.AddSingleton<IGreetingService, GreetingService>();//per applicaiton lifetime
+//Register dependency
+
+var serviceProvider = services.BuildServiceProvider();
+//Build provider for service
+
+var greetObj = serviceProvider.GetRequiredService<IGreetingService>();
+//provider provide me a service
+
+greetObj.Greet("Ashwin");
+
+Console.ReadKey();
+
+//Realworld example
+//in e commerce portal, there will be option for multiple payment methods.
+//It will be difficult if one payment method is hard coded.
+
+//Purpose of Dependency injection is decoupling
+
+//If there are 2 classes with same name then add namespace before to that.
