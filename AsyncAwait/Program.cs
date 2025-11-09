ecd794ae938a555dcdc3ad191c1728d6ead9ec50
Ashwin Pc <Ashwin Pc@DESKTOP-NRN0EQ3>
2025-11-09 17:25:59 +0530

    async await

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Starting");
+string result = await GetData();
+Console.WriteLine(result);
+Console.WriteLine("End");
+
+async Task<string> GetData()
+{
+    Console.WriteLine("Fetch data");
+    await Task.Delay(30000);
+    return "Data received";
+}
